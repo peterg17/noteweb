632cfa72c16c42733406cfa13106981f297f6be1
Peter Griggs <pgriggs@mit.edu>
2020-01-03 17:05:01 -0800

    Added placeholder graph visualization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Graph } from '@vx/network';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+const nodes = [
+  { x: 50, y: 20 },
+  { x: 200, y: 300 },
+  { x: 300, y: 40 }
+];
+
+const links = [
+  { source: nodes[0], target: nodes[1] },
+  { source: nodes[1], target: nodes[2] },
+  { source: nodes[2], target: nodes[0] }
+];
+
+const graph = {
+  nodes,
+  links
+};
 
 class TopBar extends React.Component {
   render() {
@@ -9,7 +30,9 @@ class TopBar extends React.Component {
       <div>
         <div className="container">
           <div className="topbar">
-            <p>Some topbar text</p>
+            <h1>Noteweb v0.0</h1>
+          </div>
+          <div className="account">
           </div>
         </div>
       </div>
@@ -21,8 +44,8 @@ class Main extends React.Component {
   render() {
     return (
       <div className="container">
-          <LeftBar />
-          <DataScreen />
+        <LeftBar />
+        <DataScreen />
       </div>
     );
   }
@@ -39,10 +62,18 @@ class LeftBar extends React.Component {
 }
 
 class DataScreen extends React.Component {
+  attachZoom() {
+    console.log("hi");
+  }
+  
   render() {
     return (
       <div className="datascreen">
-        <p>Some main datascreen stuff</p>
+        {/* <p>Some main datascreen stuff</p> */}
+        <svg width="500" height="500">
+          <rect width={500} height={500} rx={14} fill="#272b4d" />
+          <Graph graph={graph} />
+        </svg>
       </div>
     );
   }
@@ -52,8 +83,8 @@ class NoteWeb extends React.Component {
   render() {
     return (
       <div className="noteweb">
-          <TopBar />
-          <Main />
+        <TopBar />
+        <Main />
       </div>
     );
   }
