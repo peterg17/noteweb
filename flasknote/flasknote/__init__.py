4d0d67de3f8e391c2e1f35484fb2495d401034b3
Victor Fink <vfink@mit.edu>
2020-01-26 17:49:25 -0500

    progess on backend

--- flasknote/flasknote/__init__.py
+++ flasknote/flasknote/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from . import db
 
 
 def create_app(test_config=None):
@@ -10,6 +11,8 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flasknote.sqlite'),
     )
+    
+    db.init_app(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
