e890e790bc0a241c946d386f560085f1bdfddd93
Victor Fink <vfink@mit.edu>
2020-04-26 17:18:46 -0400

    frontend and backend can now effectively communicate

--- noteweb/src/NoteWeb.js
+++ noteweb/src/NoteWeb.js
@@ -54,6 +54,66 @@ export default class NoteWeb extends Component {
     //   <Edge key="17" id= "17" from="emporer" to="luke" />,
     //   <Edge key="18" id= "18" from="lando" to="han" />
     // ];
+    let nodes = [
+      { key: "node0", id: "0", label: "Sample Title 1", content: "This is sample note 1", created:new Date().getTime()},
+      { key: "node1", id: "1", label: "Sample Title 2", content: "This is sample note 2", created:new Date().getTime()},
+      { key: "node2", id: "2", label: "Sample Title 3", content: "This is sample note 3", created:new Date().getTime()}
+    ]
+    
+    let edges = [
+       { key: "edge0", id: "0", from:"0", to:"1", created:new Date().getTime()},
+       { key: "edge0", id: "1", from:"0", to:"2", created:new Date().getTime()}
+    ]
+    // let nodes = [
+    //   { key: "node0", id: "0", label: "Darth Vader", content: "The chosen one. Brought balance to the force. Before me thousands of Jedi, only two Sith. After me, two Jedi and two Sith. That's what you call balanced.", created:new Date().getTime()},
+    //   { key: "node1", id: "1", label: "Emporer Palpatine" , content: "This is the Dark Lord of the Sith", created:new Date().getTime() },
+    //   { key: "node2", id: "2", label: "Luke Skywalker" , content: "This is the Galaxy's only hope.", created:new Date().getTime()},
+    //   { key: "node3", id: "3", label: "Leia Organa", content: "This is the girl who is always counted out but comes up big.", created:new Date().getTime()},
+    //   { key: "node4", id: "4", label: "Han Solo" , content: "What is this amateur hour?", created:new Date().getTime() },
+    //   { key: "node5", id: "5", label: "Chewbacca", content: "Ahhhhhhhhhhhhh!", created:new Date().getTime()},
+    //   { key: "node6", id: "6", label: "Obi-Wan Kenobi" , content: "Why did you try it Anakin? I had the high ground. And I've used that exact trick on Darth Maul which I must have told you about. Did you think I wouldn't see that shit coming? Come on young padawan.", created:new Date().getTime() },
+    //   { key: "node7", id: "7", label: "Greedo", content: "The green guy who was just too slow for Han Solo. Or was I innacurate? Did I shoot or not? Idk George Lucas keeps editing my only scene", created:new Date().getTime()},
+    //   { key: "node8", id: "8", label: "Yoda" , content: "That baby me am not. Look nothing like him, I did. More handsome I am.", created:new Date().getTime() },
+    //   { key: "node9", id: "9", label: "Boba Fett", content: "Still being digested to this day in The Great Pit of Carkoon", created:new Date().getTime()},
+    //   { key: "node10", id: "10", label: "Jabba The Hutt" , content: "Me want solo on my wall.", created:new Date().getTime() },
+    //   { key: "node11", id: "11", label: "Lando Calrissian", content: "I'm in it for the money, and the women, and the greater good, but definitely the money.", created:new Date().getTime()}
+    // ]
+    // 
+    // let edges = [
+    //   { key: "edge0", id: "0", from:"8", to:"5", created:new Date().getTime()},
+    //   { key: "edge1", id: "1", from:"0", to:"2", created:new Date().getTime()},
+    //   { key: "edge2", id: "2", from:"0", to:"3", created:new Date().getTime()},
+    //   { key: "edge3", id: "3", from:"4", to:"3", created:new Date().getTime()},
+    //   { key: "edge4", id: "4", from:"4", to:"5", created:new Date().getTime()},
+    //   { key: "edge5", id: "5", from:"6", to:"2", created:new Date().getTime()},
+    //   { key: "edge6", id: "6", from:"3", to:"2", created:new Date().getTime()},
+    //   { key: "edge7", id: "7", from:"0", to:"6", created:new Date().getTime()},
+    //   { key: "edge8", id: "8", from:"6", to:"8", created:new Date().getTime()},
+    //   { key: "edge9", id: "9", from:"0", to:"8", created:new Date().getTime()},
+    //   { key: "edge10", id: "10", from:"10", to:"4", created:new Date().getTime()},
+    //   { key: "edge11", id: "11", from:"10", to:"9", created:new Date().getTime()},
+    //   { key: "edge12", id: "12", from:"9", to:"4", created:new Date().getTime()},
+    //   { key: "edge13", id: "13", from:"4", to:"7", created:new Date().getTime()},
+    //   { key: "edge14", id: "14", from:"10", to:"7", created:new Date().getTime()},
+    //   { key: "edge15", id: "15", from:"8", to:"2", created:new Date().getTime()},
+    //   { key: "edge16", id: "16", from:"1", to:"0", created:new Date().getTime()},
+    //   { key: "edge17", id: "17", from:"1", to:"2", created:new Date().getTime()},
+    //   { key: "edge18", id: "18", from:"11", to:"4", created:new Date().getTime()}
+    // ]
+    let graph = {nodes:nodes,edges:edges};
+    let thisItem = graph.nodes[0];
+    this.state = {id: thisItem.id,  title: thisItem.label, note: thisItem.content, nodes: nodes, edges: edges, graph:graph};
+    // this.state = {id: thisItem.props.id,  title: thisItem.props.label, note: thisItem.props.content, nodes: nodes, edges: edges, items:items, graph:{nodes:nodes, edges:edges}};
+    this.setNote = this.setNote.bind(this);
+    this.fetchNotesAndEdges = this.fetchNotesAndEdges.bind(this);
+    this.setTitle = this.setTitle.bind(this);
+    this.getNote = this.getNote.bind(this);
+    this.addNote = this.addNote.bind(this);
+    this.computeItems = this.computeItems.bind(this);
+    this.setNodes = this.setNodes.bind(this);
+  }
+  
+  fetchNotesAndEdges(){
     let fetch_nodes = fetch('http://localhost:5000/note/get_all',{
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
         headers: {
@@ -63,59 +123,8 @@ export default class NoteWeb extends Component {
         return response.json();
       })
       .then((myJson) => {
-        console.log(fetch_nodes.length == 0)
-        console.log(myJson);
+        this.setNodes(myJson['nodes'],myJson['edges']);
       });
-    console.log("poop")
-    console.log(fetch_nodes);
-    console.log("jews")
-    let nodes = [
-      { key: "node0", id: "0", label: "Darth Vader", content: "The chosen one. Brought balance to the force. Before me thousands of Jedi, only two Sith. After me, two Jedi and two Sith. That's what you call balanced.", created:new Date().getTime()},
-      { key: "node1", id: "1", label: "Emporer Palpatine" , content: "This is the Dark Lord of the Sith", created:new Date().getTime() },
-      { key: "node2", id: "2", label: "Luke Skywalker" , content: "This is the Galaxy's only hope.", created:new Date().getTime()},
-      { key: "node3", id: "3", label: "Leia Organa", content: "This is the girl who is always counted out but comes up big.", created:new Date().getTime()},
-      { key: "node4", id: "4", label: "Han Solo" , content: "What is this amateur hour?", created:new Date().getTime() },
-      { key: "node5", id: "5", label: "Chewbacca", content: "Ahhhhhhhhhhhhh!", created:new Date().getTime()},
-      { key: "node6", id: "6", label: "Obi-Wan Kenobi" , content: "Why did you try it Anakin? I had the high ground. And I've used that exact trick on Darth Maul which I must have told you about. Did you think I wouldn't see that shit coming? Come on young padawan.", created:new Date().getTime() },
-      { key: "node7", id: "7", label: "Greedo", content: "The green guy who was just too slow for Han Solo. Or was I innacurate? Did I shoot or not? Idk George Lucas keeps editing my only scene", created:new Date().getTime()},
-      { key: "node8", id: "8", label: "Yoda" , content: "That baby me am not. Look nothing like him, I did. More handsome I am.", created:new Date().getTime() },
-      { key: "node9", id: "9", label: "Boba Fett", content: "Still being digested to this day in The Great Pit of Carkoon", created:new Date().getTime()},
-      { key: "node10", id: "10", label: "Jabba The Hutt" , content: "Me want solo on my wall.", created:new Date().getTime() },
-      { key: "node11", id: "11", label: "Lando Calrissian", content: "I'm in it for the money, and the women, and the greater good, but definitely the money.", created:new Date().getTime()}
-    ]
-    
-    let edges = [
-      { key: "edge0", id: "0", from:"8", to:"5", created:new Date().getTime()},
-      { key: "edge1", id: "1", from:"0", to:"2", created:new Date().getTime()},
-      { key: "edge2", id: "2", from:"0", to:"3", created:new Date().getTime()},
-      { key: "edge3", id: "3", from:"4", to:"3", created:new Date().getTime()},
-      { key: "edge4", id: "4", from:"4", to:"5", created:new Date().getTime()},
-      { key: "edge5", id: "5", from:"6", to:"2", created:new Date().getTime()},
-      { key: "edge6", id: "6", from:"3", to:"2", created:new Date().getTime()},
-      { key: "edge7", id: "7", from:"0", to:"6", created:new Date().getTime()},
-      { key: "edge8", id: "8", from:"6", to:"8", created:new Date().getTime()},
-      { key: "edge9", id: "9", from:"0", to:"8", created:new Date().getTime()},
-      { key: "edge10", id: "10", from:"10", to:"4", created:new Date().getTime()},
-      { key: "edge11", id: "11", from:"10", to:"9", created:new Date().getTime()},
-      { key: "edge12", id: "12", from:"9", to:"4", created:new Date().getTime()},
-      { key: "edge13", id: "13", from:"4", to:"7", created:new Date().getTime()},
-      { key: "edge14", id: "14", from:"10", to:"7", created:new Date().getTime()},
-      { key: "edge15", id: "15", from:"8", to:"2", created:new Date().getTime()},
-      { key: "edge16", id: "16", from:"1", to:"0", created:new Date().getTime()},
-      { key: "edge17", id: "17", from:"1", to:"2", created:new Date().getTime()},
-      { key: "edge18", id: "18", from:"11", to:"4", created:new Date().getTime()}
-    ]
-    console.log(fetch_nodes.length == 0)
-    console.log(fetch_nodes === undefined)
-    let graph = {nodes:nodes,edges:edges};
-    let thisItem = graph.nodes[0];
-    this.state = {id: thisItem.id,  title: thisItem.label, note: thisItem.content, nodes: nodes, edges: edges, graph:graph};
-    // this.state = {id: thisItem.props.id,  title: thisItem.props.label, note: thisItem.props.content, nodes: nodes, edges: edges, items:items, graph:{nodes:nodes, edges:edges}};
-    this.setNote = this.setNote.bind(this);
-    this.setTitle = this.setTitle.bind(this);
-    this.getNote = this.getNote.bind(this);
-    this.addNote = this.addNote.bind(this);
-    this.computeItems = this.computeItems.bind(this);
   }
   
   getNote(newId){
@@ -141,6 +150,10 @@ export default class NoteWeb extends Component {
     /**
      * Computes the new graph to be 
      */
+    console.log("compute")
+    console.log(id)
+    console.log(nodes)
+    console.log(edges)
     let graph = {nodes: [], edges: []};
     graph.nodes.push(nodes.find(x => x.id === id));
     for (let i = 0; i < edges.length; i++) {
@@ -178,20 +191,20 @@ export default class NoteWeb extends Component {
       }));
   }
   
-  resetGraph(){
+  setNodes(newNodes,newEdges){
     /**
      * resets the graph and displays all nodes
      */
     console.log("reset");
     // let stateCopy = JSON.parse(JSON.stringify(this.state));
     let stateCopy = Object.assign({}, this.state);
-    let graph = {nodes:stateCopy.nodes, edges:stateCopy.edges};
+    let graph = {nodes:newNodes, edges:newEdges};
     this.setState(state => ({
         id:stateCopy.id,
         title: stateCopy.title,
         note: stateCopy.note,
-        nodes: stateCopy.nodes,
-        edges: stateCopy.edges,
+        nodes: newNodes,
+        edges: newEdges,
         graph: graph
       }));
   }
@@ -240,7 +253,7 @@ export default class NoteWeb extends Component {
       <div className="note-web">
         <TopBar key="top"/>
         <div className="main-content">
-          <LeftBar key="left" getNote={this.getNote} addNote={this.addNote} state={this.state}/>
+          <LeftBar key="left" nodes={this.state.nodes} getNote={this.getNote} addNote={this.addNote} state={this.state} fetchNotesAndEdges={this.fetchNotesAndEdges}/>
           <DataScreen key="data" graph={this.state.graph} getNote={this.getNote}/>
           {/* <GraphWrapper /> */}
           <NoteScreen key="note" title={this.state.title} note={this.state.note} setNote={this.setNote} setTitle={this.setTitle}/>
